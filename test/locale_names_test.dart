d98ffa6193ddb66f3dc94f36ea2076b2455f5d6c
Wenqi Li <firelands128@gmail.com>
2023-07-31 17:26:09 +0800

    Use 'const' with the constructor

--- test/locale_names_test.dart
+++ test/locale_names_test.dart
@@ -6,38 +6,35 @@ import 'package:locale_names/locale_names.dart';
 
 void main() {
   test('get default display names', () {
-    Locale fr = Locale.fromSubtags(languageCode: "fr");
+    Locale fr = const Locale.fromSubtags(languageCode: "fr");
     expect(fr.defaultDisplayLanguage, "French");
     expect(fr.defaultDisplayLanguageScript, "French");
 
-    Locale zh = Locale.fromSubtags(
-        languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
+    Locale zh = const Locale.fromSubtags(languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
     expect(zh.defaultDisplayLanguage, "Chinese");
     expect(zh.defaultDisplayLanguageScript, "Chinese (Simplified)");
     expect(zh.defaultDisplayCountry, "China");
   });
 
   test('get native display names', () {
-    Locale en = Locale.fromSubtags(languageCode: "en", countryCode: "US");
+    Locale en = const Locale.fromSubtags(languageCode: "en", countryCode: "US");
     expect(en.nativeDisplayLanguage, "English");
     expect(en.nativeDisplayLanguageScript, "English");
     expect(en.nativeDisplayCountry, "United States");
 
-    Locale fr = Locale.fromSubtags(languageCode: "fr");
+    Locale fr = const Locale.fromSubtags(languageCode: "fr");
     expect(fr.nativeDisplayLanguage, "français");
     expect(fr.nativeDisplayLanguageScript, "français");
 
-    Locale zh = Locale.fromSubtags(
-        languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
+    Locale zh = const Locale.fromSubtags(languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
     expect(zh.nativeDisplayLanguage, "中文");
     expect(zh.nativeDisplayLanguageScript, "中文（简体）");
     expect(zh.nativeDisplayCountry, "中国");
   });
 
   test('get display names in a specific locale', () {
-    Locale fr = Locale.fromSubtags(languageCode: "fr");
-    Locale zh = Locale.fromSubtags(
-        languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
+    Locale fr = const Locale.fromSubtags(languageCode: "fr");
+    Locale zh = const Locale.fromSubtags(languageCode: 'zh', scriptCode: "Hans", countryCode: 'CN');
     expect(zh.displayLanguageIn(fr), "chinois");
     expect(zh.displayLanguageScriptIn(fr), "chinois (simplifié)");
     expect(zh.displayCountryIn(fr), "Chine");
